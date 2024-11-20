f53a14274f57999dba51587f168758ab180c5932
JDoc <jdoc@mail.com>
2024-11-20 16:41:12 -0500

    refactor sample triggers, delete unimplemented

--- backends/aws-sagemaker/__main__.py
+++ backends/aws-sagemaker/__main__.py
@@ -1,36 +1,81 @@
+"""An AWS Python Pulumi program"""
+
+import iam
 import pulumi
 import pulumi_aws as aws
 
-# Create an IAM role for the Lambda function
-role = aws.iam.Role(
-    "lambda-role",
-    assume_role_policy="""{
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Action": "sts:AssumeRole",
-                "Principal": { "Service": "lambda.amazonaws.com" },
-                "Effect": "Allow"
-            }
-        ]
-    }""",
-)
+region = aws.config.region
 
-aws.iam.RolePolicyAttachment(
-    "lambda-basic-policy",
-    role=role.id,
-    policy_arn="arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
-)
+custom_stage_name = "example"
 
-# Create the Lambda function
-lambda_function = aws.lambda_.Function(
-    "hello-world",
+##################
+## Lambda Function
+##################
+
+# Create a Lambda function, using code from the `./lambda` folder.
+lambda_func = aws.lambda_.Function(
+    "wandb-pml-lambda",
+    role=iam.lambda_role.arn,
     runtime="python3.9",
-    handler="handler.lambda_handler",
-    code=pulumi.AssetArchive({".": pulumi.FileArchive("./")}),
-    role=role.arn,
-    environment={"variables": {"MESSAGE": "Hello from Pulumi!"}},
+    handler="main.handler",
+    code=pulumi.AssetArchive({".": pulumi.FileArchive("./lambda")}),
 )
 
-# Export the Lambda function's ARN;
-pulumi.export("function_arn", lambda_function.arn)
+
+#########################################################################
+# Create an HTTP API and attach the lambda function to it
+##    /{proxy+} - passes all requests through to the lambda function
+##
+#########################################################################
+
+http_endpoint = aws.apigatewayv2.Api("http-api-pulumi-example", protocol_type="HTTP")
+
+http_lambda_backend = aws.apigatewayv2.Integration(
+    "example",
+    api_id=http_endpoint.id,
+    integration_type="AWS_PROXY",
+    connection_type="INTERNET",
+    description="Lambda example",
+    integration_method="POST",
+    integration_uri=lambda_func.arn,
+    passthrough_behavior="WHEN_NO_MATCH",
+)
+
+url = http_lambda_backend.integration_uri
+
+http_route = aws.apigatewayv2.Route(
+    "example-route",
+    api_id=http_endpoint.id,
+    route_key="ANY /{proxy+}",
+    target=http_lambda_backend.id.apply(lambda targetUrl: "integrations/" + targetUrl),
+)
+
+http_stage = aws.apigatewayv2.Stage(
+    "example-stage",
+    api_id=http_endpoint.id,
+    route_settings=[
+        {
+            "route_key": http_route.route_key,
+            "throttling_burst_limit": 1,
+            "throttling_rate_limit": 0.5,
+        }
+    ],
+    auto_deploy=True,
+)
+
+# Give permissions from API Gateway to invoke the Lambda
+http_invoke_permission = aws.lambda_.Permission(
+    "api-http-lambda-permission",
+    action="lambda:invokeFunction",
+    function=lambda_func.name,
+    principal="apigateway.amazonaws.com",
+    source_arn=http_endpoint.execution_arn.apply(lambda arn: arn + "*/*"),
+)
+
+# See "Outputs" for (Inputs and Outputs)[https://www.pulumi.com/docs/intro/concepts/inputs-outputs/] the usage of the pulumi.Output.all function to do string concatenation
+pulumi.export(
+    "apigatewayv2-http-endpoint",
+    pulumi.Output.all(http_endpoint.api_endpoint, http_stage.name).apply(
+        lambda values: values[0] + "/" + values[1] + "/"
+    ),
+)
--- backends/azure/__init__.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-
-def main(req):
-    logging.info("Hello, World from Azure Functions!")
-    return "Hello, World from Azure Functions!"
